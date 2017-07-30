c841277e6477deeda8d5b3cd9d99eea7880408fb
dhananjay <dhanu.hyd@gmail.com>
2017-07-30 16:48:58 +0530

    2nd line added

--- src/com/djr/main/GitMan.java
+++ src/com/djr/main/GitMan.java
@@ -3,5 +3,6 @@ package com.djr.main;
 public class GitMan {
     public static  void main(String arg[]){
         System.out.println("1st line added");
+        System.out.println("2nd line added");
     }
 }
